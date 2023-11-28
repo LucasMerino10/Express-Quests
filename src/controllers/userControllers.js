f6199f90d5d1759e7f2c884c6fc17abfb73f38e9
LucasMerino10 <lucas.merino9@gmail.com>
2023-11-28 15:48:21 +0100

    quest_4

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -67,9 +67,28 @@ const putUser = (req, res) => {
     });
 };
 
+const deleteUser = (req, res) => {
+  const id = req.params.id;
+
+  database
+    .query("DELETE FROM users WHERE id = ?", id)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
 module.exports = {
   getUsers,
   getUserById,
   postUser,
   putUser,
+  deleteUser,
 };
