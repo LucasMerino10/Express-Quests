d4d535f3b5df45cba65c2b50c9cb79f2b789899e
LucasMerino10 <lucas.merino9@gmail.com>
2023-11-28 14:05:09 +0100

    quest 03.2

--- src/app.js
+++ src/app.js
@@ -6,14 +6,16 @@ app.use(express.json());
 
 const movieControllers = require("./controllers/movieControllers");
 const userControllers = require("./controllers/userControllers");
+const validateMovie = require("./middlewares/validateMovie");
+const validateUser = require("./middlewares/validateUser");
 
 app.get("/api/movies", movieControllers.getMovies);
 app.get("/api/movies/:id", movieControllers.getMovieById);
 app.get("/api/users", userControllers.getUsers);
 app.get("/api/users/:id", userControllers.getUserById);
-app.post("/api/movies", movieControllers.postMovie);
-app.post("/api/users", userControllers.postUser);
-app.put("/api/movies/:id", movieControllers.putMovie);
-app.put("/api/users/:id", userControllers.putUser);
+app.post("/api/movies", validateMovie, movieControllers.postMovie);
+app.post("/api/users", validateUser, userControllers.postUser);
+app.put("/api/movies/:id", validateMovie, movieControllers.putMovie);
+app.put("/api/users/:id", validateUser, userControllers.putUser);
 
 module.exports = app;
--- tests/users.test.js
+++ tests/users.test.js
@@ -82,7 +82,7 @@ describe("POST /api/users", () => {
       .post("/api/users")
       .send(userWithMissingProps);
 
-    expect(response.status).toEqual(500);
+    expect(response.status).toEqual(422);
   });
 });
 
@@ -156,7 +156,7 @@ describe("PUT /api/users/:id", () => {
       .put("/api/users/1")
       .send(userWithMissingProps);
 
-    expect(response.status).toEqual(500);
+    expect(response.status).toEqual(422);
   });
 
   it("should return no user", async () => {
